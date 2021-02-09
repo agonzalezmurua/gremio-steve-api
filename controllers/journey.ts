06ed65206e84609dc6a2f8996c362821d472b8e8
Agustín González <8430727+agonzalezmurua@users.noreply.github.com>
2021-02-09 00:18:17 -0300

    wip: journey creatinon

--- controllers/journey.ts
+++ controllers/journey.ts
@@ -2,6 +2,7 @@ import * as mongoose from "mongoose";
 import { Request, Response } from "express";
 import {
   ApiOperationGet,
+  ApiOperationPost,
   ApiPath,
   SwaggerDefinitionConstant,
 } from "swagger-express-ts";
@@ -9,12 +10,12 @@ import {
 import BaseController from "./_base";
 
 import Journey from "../models/journey";
-import JourneySchema, { IJourneyDocument } from "../schemas/journey";
+import JourneySchema, { IJourney, IJourneyDocument } from "../schemas/journey";
 
 const MongooseModel = mongoose.model("Journey", JourneySchema);
 
 @ApiPath({
-  path: "/journey",
+  path: "/journeys",
   name: "Journeys",
 })
 class JourneyController extends BaseController<IJourneyDocument> {
@@ -35,13 +36,43 @@ class JourneyController extends BaseController<IJourneyDocument> {
       query: { search = "" },
     } = req;
 
-    this.model
-      .fuzzySearch(search as string)
+    MongooseModel.fuzzySearch(search as string)
       .select("-confidenceScore")
       .exec()
       .then((journeys) =>
         res.json(journeys.map((journey) => new Journey(journey)))
       );
+  }
+
+  @ApiOperationPost({
+    parameters: {
+      body: {
+        model: "Journey",
+      },
+    },
+    responses: {
+      200: {
+        model: "Journey",
+        type: SwaggerDefinitionConstant.OBJECT,
+      },
+    },
+    security: {
+      ensureAuthenticated: [],
+    },
+  })
+  public create(req: Request<null, null, IJourney>, res: Response) {
+    const {
+      artist,
+      banner_url,
+      beatmaps = [],
+      metadata,
+      modes = [],
+    } = req.body;
+    new MongooseModel({
+      organized: req.user._id,
+    })
+      .save({ validateBeforeSave: true })
+      .then((journey) => res.json(journey));
   }
 }
 
--- routes/journeys.ts
+++ routes/journeys.ts
@@ -1,8 +1,10 @@
 import * as express from "express";
 import JourneyController from "../controllers/journey";
+import ensureAuthenticated from "../middlewares/ensureAuthenticated";
 
 const router = express.Router();
 
 router.get("/", JourneyController.search);
+router.post("/", ensureAuthenticated, JourneyController.create);
 
 export default router;
